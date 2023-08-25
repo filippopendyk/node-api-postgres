9e7f0dd24ae54337929430c202fb7e1d31f166e3
filippopendyk <filipos28511@gmail.com>
2023-08-25 05:04:30 -0400

    creates createUser

--- queries.js
+++ queries.js
@@ -26,3 +26,14 @@ const getUserById = (request, response) => {
         response.status(200).json(results.rows);
     })
 }
+
+const createUser = (request, response) => {
+    const { name, email } = request.body;
+
+    pool.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
+        if(error){
+            throw Error;
+        }
+        response.status(201).send(`User added with ID: ${results.rows[0].id}`);
+    });
+}
